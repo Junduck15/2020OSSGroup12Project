ca6699f2db98b5959ed446bb9fe6630173abec17
Junduck15 <63302994+Junduck15@users.noreply.github.com>
2020-04-29 23:23:39 +0900

    Minor typo changes

--- studentinfo.h
+++ studentinfo.h
@@ -5,19 +5,19 @@ typedef struct{
     char name[20];;
     int num;
     char major[20];
-} info;
+} Info;
 
 int curcount, count, menu;
 
-int selectmenu();
-void readinfo(info s);
-int addinfo(info *s);
-int updateinfo(info *s);
-int deleteinfo(info *s);
-void listsinfo(info *s, int count);
-int selectdatano(info *s, int count);
-void savedata(info *s, int count);
-int loaddata(info s[]);
+int selectMenu();
+void readInfo(info s);
+int addInfo(info *s);
+int updateInfo(info *s);
+int deleteInfo(info *s);
+void listsInfo(info *s, int count);
+int selectDataNo(info *s, int count);
+void saveData(info *s, int count);
+int loadDta(info s[]);
 void searchByName(info *s, int count);
 void searchByNum(info *s, int count);
 void searchByMajor(info *s, int count);
