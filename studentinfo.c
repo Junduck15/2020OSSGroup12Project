009aa0b159fa92ad928a2ef457b2d3af4c5154e8
Junseok Park <21700302@handong.edu>
2020-05-03 21:42:35 +0900

    Added functions in studentinfo.c

--- studentinfo.c
+++ studentinfo.c
@@ -14,3 +14,32 @@ int addInfo(info *s){
 	return 1; 
 
 }
+void listIroduct(info *s,int count){
+
+    printf("\nNo. Name    StudentID   Major\n");
+    printf("================================\n");
+    for(int i=0; i<count; i++){
+        if( s[i].studentID == -1) continue;
+        printf("%2d.", i+1);
+        readProduct(s[i]);
+    }
+    printf("\n");
+}
+
+int selectDataNo(info *s, int count){
+    int no;
+    listProduct(s,count);
+    printf("번호는 (취소:0)?");
+    scanf("%d",&no);
+    getchar();
+    return no;
+}
+
+void readInfo (info s) {
+if (s.studentID == -1){
+printf ("학생 정보가 없음\n");
+return;
+}
+printf("%s %3d %[^\n]\n", s.name, &s.studentID, s.major);
+}
+
